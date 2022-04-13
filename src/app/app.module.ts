e7021a7ca2a353c42ae255855d8ebb5b2f8b3c11
Martín <marrtingauna@gmail.com>
2022-04-13 07:34:33 -0300

    [develop] Importación de angular material, configuracion materialModule y appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//BrowserAnimationsModule es requerido para las animaciones
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from './app.component';
+import { MeterialModule } from './material.module';
 import { SalesPersonListComponent } from './sales-person-list/sales-person-list.component';
 
+//Declaraciones de los componentes que se usaran en el modulo
+//Importacion de los componentes que se usaran en el modulo
 @NgModule({
   declarations: [
     AppComponent,
     SalesPersonListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MeterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+//Modulo de Angular donde importamos las clases de Material, usa una matriz de imports donde se especifica que se importa el modulo de Material
+//y una matriz de exports donde se especifica que se exporta el modulo de Material
+@NgModule({
+    imports: [],
+    exports: [],
+})
+
+//Material Module sera un modulo que contiene todos los componentes de material y exporta una variable de tipo MaterialModule
+//Administrar importaciones de material en un solo archivo separado pero aun tener acceso.
+export class MeterialModule {
+}
