075ab96d818915389cfc8384234ba1e7b390a4c4
Martín <marrtingauna@gmail.com>
2022-05-31 22:24:30 -0300

    [develop] Soporte de paginacion en servicio del producto

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,21 +6,31 @@ import { Product } from '../common/product';
 import { ProductCategory } from '../common/product-category';
 /*Para inyectar un servicio en cualquier componente, es como una clase/componente padre que lo podes llamar de cualquier lado */
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  
   /*URL base de servicio a llamar - Direccion de API REST*/
   private baseUrl = 'http://localhost:8080/api/products';
 
   private categoryUrl = 'http://localhost:8080/api/product-category';
 
   /* Angular tiene un marco de inyección de dependencia, HttpClient es el inyectable */
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   /**--------------------------------MÉTODOS------------------------------------------ */
 
   // Método para obtener todos los productos
+  getProductsListPaginate(
+    thePage: number,
+    thePageSize: number,
+    categoryID: number
+  ): Observable<GetResponseProducts> {
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryID}
+    &page=${thePage}&size=${thePageSize}`;
+    // Se retorna un observable de productos (Product[]) que se obtiene de la API REST
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   getProductsList(categoryID: number): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryID}`;
     // Se retorna un observable de productos (Product[]) que se obtiene de la API REST
@@ -35,42 +45,44 @@ export class ProductService {
   }
 
   // Metodo para obtener un producto por su categoria
-  getProductCategories():Observable<ProductCategory[]> {
-    return this.httpClient.get<GetResponseProductsCategories>(this.categoryUrl).pipe(
-      map(response => response._embedded.productCategory)// Mapeo de la respuesta de la API REST
-    );
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient
+      .get<GetResponseProductsCategories>(this.categoryUrl)
+      .pipe(
+        map((response) => response._embedded.productCategory) // Mapeo de la respuesta de la API REST
+      );
   }
 
   // Metodo para obtener un producto por su id
   getProduct(theProductId: number): Observable<Product> {
     const productUrl = `${this.baseUrl}/${theProductId}`;
     // Se retorna un observable de un producto (Product) que se obtiene de la API REST
-    return this.httpClient.get<Product>(productUrl)
+    return this.httpClient.get<Product>(productUrl);
   }
 
   // Metodo refactorizado para obtener los productos(Utilizado para metodo de busca y listado)
   private getProducts(searchUrl: string): Observable<Product[]> {
-    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
-      map(response => response._embedded.products)
-    );
+    return this.httpClient
+      .get<GetResponseProducts>(searchUrl)
+      .pipe(map((response) => response._embedded.products));
   }
-
-  
-
-  
-
-  
-
 }
 
 //Interface para obtener la respuesta del servicio en formato JSON y convertirlo a Product[]
+// Agregando soporte para paginacion de productos
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
-  }
+  };
+  page: {
+    size: number;
+    totalElements: number;
+    totalPages: number;
+    number: number;
+  };
 }
 interface GetResponseProductsCategories {
   _embedded: {
     productCategory: ProductCategory[];
-  }
+  };
 }
