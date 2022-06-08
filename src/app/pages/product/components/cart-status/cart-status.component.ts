0d351d3f8e5a77b7a6c3521118a53a48ba86d75d
Martín <73865955+Marttingauna@users.noreply.github.com>
2022-06-08 18:32:47 -0300

    [develop] Obteniendo el total de la cantidad de productos y el total de la suma de los precios de los productos en el carrito de compras desde el servicio

--- src/app/pages/product/components/cart-status/cart-status.component.ts
+++ src/app/pages/product/components/cart-status/cart-status.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../../../services/cart.service';
 
 @Component({
   selector: 'app-cart-status',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartStatusComponent implements OnInit {
 
-  constructor() { }
+  totalPrice: number = 0.00;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.updateCartStatus();
   }
 
+  // Obteniendo el total de la cantidad de productos y el total de la suma de los precios de los productos en el carrito de compras desde el servicio
+  updateCartStatus() {
+    // Actualizar el precio total del carrito de compras
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+    // Actualizar la cantidad total del carrito de compras.
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
+
+  }
 }
+
