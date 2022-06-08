166a3330cb7d901beabfa55b28c0431c9a4592fb
Martín <marrtingauna@gmail.com>
2022-06-08 02:16:28 -0300

    [develop] Creando componente para el estado del carrito actual

--- src/app/pages/product/product-list.module.ts
+++ src/app/pages/product/product-list.module.ts
@@ -11,6 +11,7 @@ import { SearchComponent } from './components/menu/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     ProductListComponent,
     ProductCategoryMenuComponent,
     SearchComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartStatusComponent
   ],
   exports:[
     ProductlistPageComponent,
