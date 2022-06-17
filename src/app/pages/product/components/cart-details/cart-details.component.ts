6404fd6ce4c927fe2515d214f91fc67df70fe486
Martín <73865955+Marttingauna@users.noreply.github.com>
2022-06-17 18:13:27 -0300

    [develop] Creando componente donde se listan los productos actuales que tiene el carrito de compra

---
+++ src/app/pages/product/components/cart-details/cart-details.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart-details',
+  templateUrl: './cart-details.component.html',
+  styleUrls: ['./cart-details.component.css']
+})
+export class CartDetailsComponent implements OnInit {
+
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.listCartDetails();
+  }
+
+  listCartDetails() {
+
+    //Obtenemos los items del carrito
+    this.cartItems = this.cartService.cartItems;
+
+    //Actualizamos el total de precio y cantidad
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    this.cartService.totalQuantity.subscribe( 
+      data => this.totalQuantity = data
+    );
+
+    //Calculo del total de precio y cantidad
+    this.cartService.computeCartTotals();
+
+}
+}
--- src/app/pages/product/product-routing.module.ts
+++ src/app/pages/product/product-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductlistPageComponent } from './components/productlist-page/productlist-page.component';
 import { ProductListComponent } from './components/product-list-grid/product-list-grid.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 const routes: Routes = [
     {
@@ -11,6 +12,7 @@ const routes: Routes = [
         children: [
             //Rutas hijas donde se iran agregando los componentes que se quieran mostrar
             { path: 'list', component: ProductListComponent },
+            { path: 'cart-details', component: CartDetailsComponent },
             { path: 'search/:keyword', component: ProductListComponent },
             { path: 'category/:id', component: ProductListComponent },
             { path: ':id', component: ProductDetailsComponent },
