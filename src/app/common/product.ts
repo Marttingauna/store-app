1c627c36f35abe5336f189a08f65c33584fe12ef
Martín <marrtingauna@gmail.com>
2022-06-23 12:27:16 -0300

    [develop] Modificando clase producto para el alta del mismo

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -11,5 +11,21 @@ export class Product {
   dateCreated: Date;
   lastUpdated: Date;
 
-  
+  constructor(
+    category: string,
+    name: string,
+    description: string,
+    unitPrice: number,
+    imageUrl: string,
+    active: boolean,
+    unitsInStock: number
+  ) {
+    this.category = category;
+    this.name = name;
+    this.description = description;
+    this.unitPrice = unitPrice;
+    this.imageUrl = imageUrl;
+    this.active = active;
+    this.unitsInStock = unitsInStock;
+  }
 }
