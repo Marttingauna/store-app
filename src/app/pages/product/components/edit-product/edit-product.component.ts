681e3b4d8915f6a56a684ba63ac079ad3f05c62a
Martín <marrtingauna@gmail.com>
2022-06-26 21:15:15 -0300

    [develop] 🚀 Agregando funcionalidad a la edicion de un producto y solo podra visualizarlo un usuario administrador

--- src/app/pages/product/components/edit-product/edit-product.component.ts
+++ src/app/pages/product/components/edit-product/edit-product.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
+import { UploadService } from 'src/app/services/upload.service';
 
 @Component({
   selector: 'app-edit-product',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProductComponent implements OnInit {
 
-  constructor() { }
+  producto: Product = null;
 
-  ngOnInit(): void {
+  constructor(
+    private productoService: ProductService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    const theProductId: number = +this.activatedRoute.snapshot.paramMap.get('id')!;
+    this.productoService.detail(theProductId).subscribe(
+      data => {
+        this.producto = data;
+      },
+      err => {
+        
+        this.router.navigate(['/']);
+      }
+    );
   }
+
+  onUpdate(): void {
+    const theProductId: number = +this.activatedRoute.snapshot.paramMap.get('id')!;
+    this.productoService.update(theProductId, this.producto).subscribe(
+      data => {
+        
+        this.router.navigate(['/product/list']);
+      },
+      err => {
+        
+        // this.router.navigate(['/']);
+      }
+    );
+  }
+
 
 }
